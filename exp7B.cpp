86c085bade63cc2250c6128bfbab8e58a79e48fd
subhamhada <subhamrao0011@gmail.com>
2024-08-07 00:27:31 +0530

    Add files via upload

---
+++ exp7B.cpp
@@ -0,0 +1,18 @@
+//subham
+//entc B2
+//23070123132
+//experiment 7
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j=0,k,l,temp;
+    cout<<"enter size of array";
+    cin>>n;
+    int a1[n],a2[n];
+    for (i=0;i<n;i++)
+    {
+        cout<<"enter element-"<<i+1<<": ";
+        cin>>a1[i];
+    }
+} 
---
+++ exp7E.cpp
@@ -0,0 +1,20 @@
+//subham
+//entc B2
+//23070123132
+//experiment 7
+#include<iostream>
+using namespace std;
+int main() {
+int a1[5], i, j;
+float sum=0, avg;
+for(i=0;i<5;i++) {
+cout<<"Enter element-"<<i+1<<":";
+cin>>a1[i];
+}
+for(j=0;j<5;j++) {
+sum = sum + a1[j];
+}
+cout<<"Sum of elements = "<<sum<<endl;
+avg = sum/5;
+cout<<"Average = "<<avg;
+}
