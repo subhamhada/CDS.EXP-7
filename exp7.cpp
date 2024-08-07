5d58202ef8cf8af68e5a65af91b75b58281c718a
subhamhada <subhamrao0011@gmail.com>
2024-08-07 09:09:08 +0530

    Add files via upload

---
+++ exp7.cpp
@@ -0,0 +1,31 @@
+//subham
+//entc B2
+//23070123132
+//experiment 7
+#include <iostream>
+using namespace std;
+int main()
+{
+    int array1[2] = {1, 2}; 
+    int array2[2] = {2, 4}; 
+    int array3[2] = {1, 3};
+
+    // Printing array1
+    for (int i = 0; i < 2; i++) 
+    { 
+        cout << array1[i] << " ";
+    }
+    cout << endl; 
+
+    // Calling out array1[1]
+    cout << "array1[1]: " << array1[1] << endl;
+
+    // Printing array2
+    for (int value : array2) 
+    { 
+        cout << value << " "; 
+    }
+    cout << endl;
+
+    return 0;
+}
